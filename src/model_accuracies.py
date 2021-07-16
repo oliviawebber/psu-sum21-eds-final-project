d0776540ca69b7317a21120fb647336d06e4a683
Layton Webber <webberlh16@gmail.com>
2021-07-16 11:24:01 -0700

    Changed accuracy file to compute cross-validation

--- src/model_accuracies.py
+++ src/model_accuracies.py
@@ -6,22 +6,27 @@ import numpy as np
 # List of all the  files we need to train models for
 fileList = ['cleveland', 'hungarian', 'long-beach-va', 'switzerland']
 
-for file in fileList:
-    # Read in the clean data
-    data = pd.read_csv('../data/{}-clean.csv'.format(file), sep=',', header=0)
-
-    # Remove the targets from the data and store them. Then convert the data
-    # into a features matrix
-    targets = np.array(data['num'])
-    data = data.drop('num', axis=1)
-    data = np.array(data)
-
-    # Split the data into training and test sets
-    trainData, testData, trainTargets, testTargets = train_test_split(data, targets, test_size=0.10, random_state=0)
-
+for model in fileList:
+    print('Model built on {}:'.format(model))
     # Load the trained model
-    rfClassifier = load('../models/{}-trained-model.joblib'.format(file))
+    rfClassifier = load('../models/{}-trained-model.joblib'.format(model))
 
-    # Print accuracy
-    print('Model for {}:'.format(file))
-    print(rfClassifier.score(testData, testTargets))
+    # Compute accuracy on each file
+    for file in fileList:
+        # Read in the clean data
+        data = pd.read_csv('../data/{}-clean.csv'.format(file), sep=',', header=0)
+
+        # Remove the targets from the data and store them. Then convert the data
+        # into a features matrix
+        targets = np.array(data['num'])
+        data = data.drop('num', axis=1)
+        data = np.array(data)
+
+        # Split the data into training and test sets
+        trainData, testData, trainTargets, testTargets = train_test_split(data, targets, test_size=0.10, random_state=0)
+
+        
+        # Print accuracy
+        print('Accuracy on file {}:'.format(file))
+        print(rfClassifier.score(testData, testTargets))
+    print('---')
